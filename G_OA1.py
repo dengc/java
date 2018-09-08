e83644f3ad5976ae341653a4b8e3f24df6648e67
Chufan Deng <jerry.dengcf@gmail.com>
2018-09-07 17:53:26 -0700

    rm

--- G_OA1.py
+++ G_OA1.py
@@ -1,9 +1,4 @@
-# you can write to stdout for debugging purposes, e.g.
-# print("this is a debug message")
-
 def solution(L):
-    # write your code in Python 3.6
-
     list = []
     for e in L :
         email = ""
@@ -32,6 +27,5 @@ def solution(L):
             oldSet.add(e)
 
     return len(oldSet)
-    pass
 
 // aa = ['a.b.+c@163.com', 'ab...c@163.com', 'abc+....b@163.com', 'x@abc.com', 'x+b@163.com', 'z.xc@abc.com', 'z.+xc@163.com', 'q.w.e@abc.com', 'g@dfg.com', 'z@163.com', 'a.s.d@ty.com', 'a.sd@ty.com', 'abc@a.sd.com', 'abc@as.d.com', 'ab.c@as.d.com', 'q.w_e@abc.com’, ‘q.w.._e+48.3q@abc.com’]
